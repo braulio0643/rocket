75d2ab69f3734fe15bb2dd57701f7f2c03594558
Braulio Solari <brauliosolari@gmail.com>
2023-11-21 21:01:00 -0300

    fix darkMode productos/index

--- js/dark.js
+++ js/dark.js
@@ -1,6 +1,9 @@
 const dark = document.querySelector(".botonDark")
 const header = document.getElementById("header")
 const footer = document.getElementById("footer")
+
+
+
 let statusDark = localStorage.getItem("dark")
 
 if(statusDark){
@@ -25,8 +28,35 @@ function applyDarkMode(status){
     if(status){
         header.classList.add("darkMode")
         footer.classList.add("darkMode")
+        document.querySelector("#body").classList.add("darkBackground")
+        document.getElementById("titulo").classList.add("yellow")
+        const titulo = document.title
+        console.log()
+        switch(titulo){
+            case "Rocket Motorcycles": 
+                const textoInicio = document.getElementById("textoInicio");
+                if(textoInicio) {textoInicio.classList.add("darkMode")} ;
+               
+            case "Productos":  
+                const tienda = document.getElementById("tienda");
+                if(tienda){tienda.classList.add("tiendaOscura")};
+                
+        }
     } else {
         header.classList.remove("darkMode")
         footer.classList.remove("darkMode")
+        document.querySelector("#body").classList.remove("darkBackground")
+        document.getElementById("titulo").classList.remove("yellow");
+        document.getElementById("titulo").classList.remove("titulo");
+        const titulo = document.title
+        switch(titulo){
+            case "Rocket Motorcycles": 
+            const textoInicio = document.getElementById("textoInicio");
+            if(textoInicio) {textoInicio.classList.remove("darkMode")} ;
+                
+            case "Productos":
+                const tienda = document.getElementById("tienda");
+            if(tienda){tienda.classList.remove("tiendaOscura")};
+        }
     }
 }
