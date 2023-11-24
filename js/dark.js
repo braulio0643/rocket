0b04bcc207d78efa40e68cda4c6dcbb2ea144d2f
Braulio Solari <brauliosolari@gmail.com>
2023-11-23 21:36:23 -0300

    darkMode completo

--- js/dark.js
+++ js/dark.js
@@ -30,24 +30,52 @@ function applyDarkMode(status){
         footer.classList.add("darkMode")
         document.querySelector("#body").classList.add("darkBackground")
         document.getElementById("titulo").classList.add("yellow")
+        
         const titulo = document.title
         console.log()
         switch(titulo){
             case "Rocket Motorcycles": 
                 const textoInicio = document.getElementById("textoInicio");
                 if(textoInicio) {textoInicio.classList.add("darkMode")} ;
-               
             case "Productos":  
                 const tienda = document.getElementById("tienda");
                 if(tienda){tienda.classList.add("tiendaOscura")};
+            case "Servicios":
+                const servicios = document.getElementById("servicios");
+                if(servicios){servicios.classList.add("serviciosOscuros")}
+            case "Galería":
+                const galeria = document.getElementById("galeria");
+                if(galeria){document.getElementById("galeria").classList.add("galeriaOscura")}
+            case "Contacto":
+                const contacto = document.getElementById("contacto");
+                if(contacto){document.getElementById("contacto").classList.add("contactoOscuro")}
+            case "Carrito":
+                const carrito = document.getElementById("carrito-container");
+                if(carrito){document.getElementById("carrito-container").classList.add("carritoOscuro")}
                 
         }
+        if(document.title== "Rocket Motorcycles"){
+            document.getElementById("botonCarrito").src= "./multimedia/carrito-yellow.png"
+            document.getElementById("botonDark").src="./multimedia/dark-yellow.png"
+            document.getElementById("ubicacion").src="./multimedia/ubicacion-yellow.png"
+            document.getElementById("ig").src="./multimedia/ig-yellow.png"
+            document.getElementById("fb").src="./multimedia/fb-yellow.png"
+            document.getElementById("gmail").src="./multimedia/gmail-yellow.png"
+        } else {
+            document.getElementById("botonCarrito").src= "../multimedia/carrito-yellow.png"
+            document.getElementById("botonDark").src="../multimedia/dark-yellow.png"
+            document.getElementById("ubicacion").src="../multimedia/ubicacion-yellow.png"
+            document.getElementById("ig").src="../multimedia/ig-yellow.png"
+            document.getElementById("fb").src="../multimedia/fb-yellow.png"
+            document.getElementById("gmail").src="../multimedia/gmail-yellow.png"
+        }
+        
+
     } else {
         header.classList.remove("darkMode")
         footer.classList.remove("darkMode")
         document.querySelector("#body").classList.remove("darkBackground")
         document.getElementById("titulo").classList.remove("yellow");
-        document.getElementById("titulo").classList.remove("titulo");
         const titulo = document.title
         switch(titulo){
             case "Rocket Motorcycles": 
@@ -57,6 +85,35 @@ function applyDarkMode(status){
             case "Productos":
                 const tienda = document.getElementById("tienda");
             if(tienda){tienda.classList.remove("tiendaOscura")};
+
+            case "Servicios":
+                const servicios = document.getElementById("servicios");
+                if(servicios){servicios.classList.remove("serviciosOscuros")}
+
+            case "Galería":
+                const galeria = document.getElementById("galeria");
+                if(galeria){document.getElementById("galeria").classList.remove("galeriaOscura")}
+            case "Contacto":
+                const contacto = document.getElementById("contacto");
+                if(contacto){document.getElementById("contacto").classList.remove("contactoOscuro")}
+            case "Carrito":
+                const carrito = document.getElementById("carrito-container");
+                if(carrito){document.getElementById("carrito-container").classList.remove("carritoOscuro")}
+        }
+        if(document.title== "Rocket Motorcycles"){
+            document.getElementById("botonCarrito").src= "./multimedia/carrito-black.png"
+            document.getElementById("botonDark").src="./multimedia/dark.png"
+            document.getElementById("ubicacion").src="./multimedia/ubicacion-black.png"
+            document.getElementById("ig").src="./multimedia/ig-black.png"
+            document.getElementById("fb").src="./multimedia/fb-black.png"
+            document.getElementById("gmail").src="./multimedia/gmail-black.png"
+        } else {
+            document.getElementById("botonCarrito").src= "../multimedia/carrito-black.png"
+            document.getElementById("botonDark").src="../multimedia/dark.png"
+            document.getElementById("ubicacion").src="../multimedia/ubicacion-black.png"
+            document.getElementById("ig").src="../multimedia/ig-black.png"
+            document.getElementById("fb").src="../multimedia/fb-black.png"
+            document.getElementById("gmail").src="../multimedia/gmail-black.png"
         }
     }
 }
